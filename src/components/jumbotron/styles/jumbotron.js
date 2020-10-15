be2782008e7d808853a556771985a62e6950c7d8
clintchu <clintchu@outlook.com>
2020-10-15 14:01:33 -0700

    Add styling to container

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -57,4 +57,10 @@ export const Image = styled.img`
     max-width: 100%;
 `;
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    @media (max-width: 1000px) {
+        ${Item}:last-of-type h2 {
+            margin-bottom: 50px;
+        }
+    }
+`;
