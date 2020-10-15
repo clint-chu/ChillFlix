54241449c8c7a4e78fc4a49ab2f5665cba93b6a9
clintchu <clintchu@outlook.com>
2020-10-15 13:51:20 -0700

    Fix indentation app.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,23 @@ import jumboData from './fixtures/jumbo';
 import Jumbotron from './components/jumbotron';
 
 function App() {
-  return (
-    <Jumbotron.Container>
+    return (
+        <Jumbotron.Container>
 
-      {jumboData.map((item) => (
-        <Jumbotron key={item.id} direction={item.direction}>
-          <Jumbotron.Pane>
-            <Jumbotron.Title>{item.title}</Jumbotron.Title>
-            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-          </Jumbotron.Pane>
-          <Jumbotron.Pane>
-            <Jumbotron.Image src={item.image} alt={item.alt} />
-          </Jumbotron.Pane>
-        </Jumbotron>
-      ))}
+        {jumboData.map((item) => (
+            <Jumbotron key={item.id} direction={item.direction}>
+            <Jumbotron.Pane>
+                <Jumbotron.Title>{item.title}</Jumbotron.Title>
+                <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+            </Jumbotron.Pane>
+            <Jumbotron.Pane>
+                <Jumbotron.Image src={item.image} alt={item.alt} />
+            </Jumbotron.Pane>
+            </Jumbotron>
+        ))}
 
-    </Jumbotron.Container>
-  );
+        </Jumbotron.Container>
+    );
 }
 
 export default App;
