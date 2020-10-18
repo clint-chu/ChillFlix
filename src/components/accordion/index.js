f483d4321fb57ceef99519ce44175288e73de846
clintchu <clintchu@outlook.com>
2020-10-18 14:25:07 -0700

    Remove Accordion.Frame

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, createContext } from 'react';
-import { Container, Frame, Title, Item, Inner, Header, Body } from './styles/accordion';
+import { Container, Title, Item, Inner, Header, Body } from './styles/accordion';
 
 const ToggleContext = createContext();
 
@@ -14,12 +14,6 @@ export default function Accordion({ children, ...restProps }) {
 Accordion.Title = function AccordionTitle({ children, ...restProps }) {
     return (
         <Title {...restProps}>{children}</Title>
-    );
-};
-
-Accordion.Frame = function AccordionFrame({ children, ...restProps }) {
-    return (
-        <Frame {...restProps}>{children}</Frame>
     );
 };
 
