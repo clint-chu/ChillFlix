22e1f9abc76c2e4841bf3a41df485c1c11cf879e
clintchu <clintchu@outlook.com>
2020-12-26 14:26:48 -0800

    Add browse, signin, and signup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './pages/home';
+import { Home, Browse, Signin, Signup } from './pages';
 import * as ROUTES from './constants/routes';
 
 export default function App() {
     return (
         <Router>
+            <Route exact path="/browse">
+                <Browse />
+            </Route>
+            <Route exact path="/signin">
+                <Signin />
+            </Route>
+            <Route exact path="/signup">
+                <Signup />
+            </Route>
             <Route exact path={ROUTES.HOME}>
                 <Home />
             </Route>
